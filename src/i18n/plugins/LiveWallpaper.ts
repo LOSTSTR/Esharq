7efc465386a4ce5238012300d9554c552ed4fd93
LOSTSTR <LOSTSTR@users.noreply.github.com>
2026-07-13 23:18:56 +0300

    feat: port LiveWallpaper (global animated wallpaper), localized

---
+++ src/i18n/plugins/LiveWallpaper.ts
@@ -0,0 +1,14 @@
+/*
+ * Vencord, a Discord client mod
+ * Copyright (c) 2026 Vendicated and contributors
+ * SPDX-License-Identifier: GPL-3.0-or-later
+ */
+
+import { definePluginI18n } from "@utils/i18n/types";
+
+export default definePluginI18n({
+    "description": {
+        "ar": "خلفية حيّة لكامل واجهة ديسكورد (صورة، GIF، فيديو). متوافقة مع ChannelWallpaper.",
+        "en": "Global wallpaper for the entire Discord interface (image, gif, video). Compatible with ChannelWallpaper."
+    }
+});
